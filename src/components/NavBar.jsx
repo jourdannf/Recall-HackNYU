886ce5ffa440366f5ddb19ff5495fe0f6961d7c5
jourdannf <47835212+jourdannf@users.noreply.github.com>
2025-02-09 11:36:19 -0500

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,7 +10,7 @@ export default function NavBar({role}) {
     if (role == "caretaker") {
         return (
             <div style={{position: "absolute", bottom: 20, color: "white", display: "flex", justifyContent: "space-between", width: "360px", margin: "auto" }}>
-                <div><Link to={`/${role}>`} style={{color: "#3E3F37", paddingLeft:"70px"}}> Home </Link></div>
+                <div><Link to={`/`} style={{color: "#3E3F37", paddingLeft:"70px"}}> Home </Link></div>
                 <div><Link to={`/caretaker/appointments/add`} style={{color: "#3E3F37", paddingRight: "70px"}}> Add</Link></div>
             </div>
         )
@@ -18,7 +18,7 @@ export default function NavBar({role}) {
     }else {
         return (
             <div style={{position: "absolute", bottom: 0, color: "white", display: "flex", justifyContent: "space-evenly", width: "360px", margin: "auto"}}>
-                <div><Link to={`/${role}>`}> Home </Link></div>
+                <div><Link to={`/`}> Home </Link></div>
                 <div><Link to={`/patient/puzzle`}> Puzzle</Link></div>
             </div>
         )
